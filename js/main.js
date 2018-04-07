8fd747f3e6e51c963762b3be3109e6648b952011
Tshiamo Bhuda <tshiamobhuda@gmail.com>
2018-04-07 12:11:26 +0200

    Modified Ajax call to make it more generic & reusable - Added roundToTwo function that rounds off number to two decimal places - added separate function to load the table, - added function to set the exchange rate to global variable

--- js/main.js
+++ js/main.js
@@ -1,30 +1,54 @@
-!(function () {
-    var xhttp = new XMLHttpRequest(),
-        currencyTable = document.getElementById('currencyTable'),
-        logo_url = "https://chasing-coins.com/api/v1/std/logo/";
+// https://stackoverflow.com/questions/2818648/using-two-xmlhttprequest-calls-on-a-page
+function sendReq(url, callbackFunction) {
+    var xmlhttp;
 
-    xhttp.open('GET','https://api.coinmarketcap.com/v1/ticker/?convert=ZAR&limit=20',true);
+    if (window.ActiveXObject) {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    } else if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    }
 
-    xhttp.onload = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            var res = JSON.parse(this.responseText);
-
-            for (var x = 0; x < res.length; x++) {
-                var tr = document.createElement('tr');
-
-                tr.innerHTML += '<td><img src="'+logo_url+res[x].symbol+'" class="currency-logo"/></td>';
-                tr.innerHTML += "<td>"+res[x].name+"</td>";
-                tr.innerHTML += "<td>"+res[x].market_cap_zar+"</td>";
-                tr.innerHTML += "<td>"+res[x].price_zar+"</td>";
-                tr.innerHTML += "<td>"+res[x].name+"</td>";
-                tr.innerHTML += "<td>"+res[x].name+"</td>";
-                tr.innerHTML += "<td>"+res[x].name+"</td>";
-                currencyTable.appendChild(tr);
-            }
-
-            // currencyTable.appendChild()
-        }
+    xmlhttp.onreadystatechange = function() {
+       if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            if (callbackFunction) callbackFunction(xmlhttp.responseText);
+       }
     };
 
-    xhttp.send();
-})();
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+// https://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-only-if-necessary
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+
+function loadTable () {
+    sendReq('https://api.coinmarketcap.com/v1/ticker/?convert=ZAR&limit=20', function (res) {
+        var logo_url = "https://chasing-coins.com/api/v1/std/logo/",
+            currencyTable = document.getElementById('currencyTable');
+
+        res = JSON.parse(res);
+        res.forEach(function (value, index) {
+            var tr = document.createElement('tr');
+
+            tr.innerHTML += '<td><img src="'+logo_url+res[index].symbol+'" class="currency-logo"/></td>';
+            tr.innerHTML += "<td>"+res[index].name+"</td>";
+            tr.innerHTML += "<td>"+roundToTwo(res[index].market_cap_usd)+" USD</td>";
+            tr.innerHTML += "<td>"+roundToTwo(res[index].price_usd)+" USD | R "+roundToTwo((res[index].price_usd*window.exchangeRate))+"</td>";
+            tr.innerHTML += "<td>"+res[index].name+"</td>";
+            tr.innerHTML += "<td>"+res[index].name+"</td>";
+            tr.innerHTML += "<td>"+res[index].name+"</td>";
+            currencyTable.appendChild(tr);
+        });
+    })
+}
+
+function setExchangeRate() {
+    sendReq('https://api.fixer.io/latest?base=USD', function (res) {
+        window.exchangeRate = roundToTwo(JSON.parse(res).rates.ZAR);
+        loadTable();
+    });
+}
+
+setExchangeRate();
