79e9f39792a87fb413911b0b6fa7fa507213ad3b
Tshiamo Bhuda <tshiamobhuda@gmail.com>
2018-04-07 22:59:05 +0200

    Refactored code to have Fetch methods that deal with just fetching data via Ajax & set it to desired variables - add function to fetch top 20 cyptocurrencies data - added function to fetch the exchange rate between rand and usd - added function to set the fetched currencies data to appropriate object for better view. - refactored loadTable method to only deal with loading data into the table. - made all Ajax calls sync

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+// for ref purposes
+var currency = {
+    logo: null,
+    name: null,
+    marketCap: null,
+    price: null,
+    avgPrice: {
+        avg: null,
+        price1: {
+            name: null,
+            url: null,
+            price: null
+        },
+        price2: {
+            name: null,
+            url: null,
+            price: null
+        },
+        price3: {
+            name: null,
+            url: null,
+            price: null
+        }
+    },
+    volume: null,
+    fullDayChange: null
+};
+
+var data = [];
+
 // https://stackoverflow.com/questions/2818648/using-two-xmlhttprequest-calls-on-a-page
 function sendReq(url, callbackFunction) {
     var xmlhttp;
@@ -9,12 +39,13 @@ function sendReq(url, callbackFunction) {
     }
 
     xmlhttp.onreadystatechange = function() {
-       if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+        console.log(xmlhttp.readyState + '|'+ url);
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
             if (callbackFunction) callbackFunction(xmlhttp.responseText);
        }
     };
 
-    xmlhttp.open("GET", url, true);
+    xmlhttp.open("GET", url, false);
     xmlhttp.send();
 }
 
@@ -23,32 +54,50 @@ function roundToTwo(num) {
     return +(Math.round(num + "e+2")  + "e-2");
 }
 
-function loadTable () {
-    sendReq('https://api.coinmarketcap.com/v1/ticker/?convert=ZAR&limit=20', function (res) {
-        var logo_url = "https://chasing-coins.com/api/v1/std/logo/",
-            currencyTable = document.getElementById('currencyTable');
-
-        res = JSON.parse(res);
-        res.forEach(function (value, index) {
-            var tr = document.createElement('tr');
-
-            tr.innerHTML += '<td><img src="'+logo_url+res[index].symbol+'" class="currency-logo"/></td>';
-            tr.innerHTML += "<td>"+res[index].name+"</td>";
-            tr.innerHTML += "<td>"+roundToTwo(res[index].market_cap_usd)+" USD</td>";
-            tr.innerHTML += "<td>"+roundToTwo(res[index].price_usd)+" USD | R "+roundToTwo((res[index].price_usd*window.exchangeRate))+"</td>";
-            tr.innerHTML += "<td>"+res[index].name+"</td>";
-            tr.innerHTML += "<td>"+res[index].name+"</td>";
-            tr.innerHTML += "<td>"+res[index].name+"</td>";
-            currencyTable.appendChild(tr);
-        });
-    })
-}
-
-function setExchangeRate() {
-    sendReq('https://api.fixer.io/latest?base=USD', function (res) {
-        window.exchangeRate = roundToTwo(JSON.parse(res).rates.ZAR);
-        loadTable();
+function fetchData () {
+    sendReq('https://api.coinmarketcap.com/v1/ticker/?limit=20', function (res) {
+        window.theData = JSON.parse(res);
     });
 }
 
-setExchangeRate();
+function fetchExchangeRate() {
+    sendReq('https://api.fixer.io/latest?base=USD', function (res) {
+        window.exchangeRate = roundToTwo(JSON.parse(res).rates.ZAR);
+    });
+}
+
+function setData () {
+    window.theData.forEach(function (value) {
+        data.push({
+            logo: "https://chasing-coins.com/api/v1/std/logo/"+value.symbol,
+            name: value.name,
+            marketCap: value.market_cap_usd,
+            price: roundToTwo((value.price_usd*window.exchangeRate)),
+            volume: value['24h_volume_usd'],
+            fullDayChange: value.percent_change_24h
+        });
+    });
+}
+
+function loadTable () {
+    var currencyTable = document.getElementById('currencyTable');
+
+    data.forEach(function (value) {
+        var tr = document.createElement('tr');
+
+        tr.innerHTML += '<td><img src="'+value.logo+'" class="currency-logo"/></td>';
+        tr.innerHTML += "<td>"+value.name+"</td>";
+        tr.innerHTML += "<td>"+value.market_cap+" USD</td>";
+        tr.innerHTML += "<td>"+value.price+"</td>";
+        tr.innerHTML += "<td>"+parseFloat('00.00')+"</td>";
+        tr.innerHTML += "<td>"+value.volume+"</td>";
+        tr.innerHTML += "<td>"+value.fullDayChange+" %</td>";
+        currencyTable.appendChild(tr);
+    });
+}
+
+fetchExchangeRate();
+fetchData();
+setData();
+
+loadTable();
